b5dd4f1b66f317a1b0cd278e340ab6bd7b004d4c
Shamsul Islam Sujon <sujon2762@gmail.com>
2020-05-03 05:29:57 +0600

    Added slider

--- preview/assets/js/scripts.js
+++ preview/assets/js/scripts.js
@@ -8,5 +8,18 @@
         $('#preloader-wrapper').delay(200).fadeOut('slow');
     });
 
+    const checkerSlider = new Swiper('.swiper-container', {
+        simulateTouch: false,
+        autoHeight: true,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-next',
+            prevEl: '.swiper-prev',
+        }
+    });
+    document.getElementById("swiper-cancel").addEventListener("click", function () {
+        checkerSlider.slideTo(0)
+    });
+
 
 })(jQuery);
